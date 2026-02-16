1d9cc2bfa06ccba4fb136fe82d2c28358b562204
MrEdDud <eduardcojocaru2016@gmail.com>
2026-02-16 17:21:02 +0000

    Nearly fixed code, still need CSS

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CST2550.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
