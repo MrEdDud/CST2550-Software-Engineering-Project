bf309dacf400cd73be686e6c84ab7f13d52b9767
Eduard <eduardcojocaru2016@gmail.com>
2026-03-15 17:26:36 +0000

    added the sidebar to all pages except auth pages

--- obj/Debug/net10.0/CST2550.AssemblyInfo.cs
+++ obj/Debug/net10.0/CST2550.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CST2550")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2770ead08a420f1fb88d97a448a380ac4824c95e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+077671de1251facb847c6a2feb2feff583bbbc68")]
 [assembly: System.Reflection.AssemblyProductAttribute("CST2550")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CST2550")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
