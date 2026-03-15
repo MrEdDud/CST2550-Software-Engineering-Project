077671de1251facb847c6a2feb2feff583bbbc68
Eduard <eduardcojocaru2016@gmail.com>
2026-03-15 16:58:21 +0000

    removed lines of code, moved parts of the auth page to authlayout

--- obj/Debug/net10.0/CST2550.AssemblyInfo.cs
+++ obj/Debug/net10.0/CST2550.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CST2550")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a614da0974e961ea23107e0907c45bb4f167ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2770ead08a420f1fb88d97a448a380ac4824c95e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CST2550")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CST2550")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
