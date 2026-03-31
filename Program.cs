c9d36d19f23267c13d6200f27c33755a813c532b
alsaiyed24 <as3799@live.mdx.ac.uk>
2026-03-31 13:03:30 +0100

    added database

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using CST2550.Components;
-// using CST2550.Services;
+using CST2550Project.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,15 +8,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Register EF Core DbContext
+builder.Services.AddDbContext<DatingAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+// Apply migrations and seed demo data
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DatingAppContext>();
+    db.Database.Migrate();
+    await db.SeedDataAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
